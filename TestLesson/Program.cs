438a9484f5ea5baaa67f7071903ce699deba3a79
VIA <base_admin@sheriff.md>
2021-06-15 11:41:15 +0300

    Лигический тип и условные оператор if и swich

--- TestLesson/Program.cs
+++ TestLesson/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 
+
 namespace TestLesson
 {
     class Program
@@ -41,30 +42,108 @@ namespace TestLesson
             #endregion
 
             #region Урок 2 /строки/ 
-            Console.OutputEncoding = System.Text.Encoding.UTF8; // для кириллицы если не отображается
-            Console.WriteLine("Привет мир!");
-            char chr = 'a'; // 2 байта
-            string str1 = "строка";
-            string str2 = "Еще одна Строка";
-            Console.WriteLine($"Длина строки ={str1.Length} символа, что составляет {str1.Length * 2} байт");
+            /*
+          Console.OutputEncoding = System.Text.Encoding.UTF8; // для кириллицы если не отображается
+          Console.WriteLine("Привет мир!");
+          char chr = 'a'; // 2 байта
+          string str1 = "строка";
+          string str2 = "Еще одна Строка";
+          Console.WriteLine($"Длина строки ={str1.Length} символа, что составляет {str1.Length * 2} байт");
 
-            Console.WriteLine($"Читаю посимвольно как из массива {str1[0]} {str1[3]} + {str1[4]}"); // "сок"
-            Console.WriteLine($"Читаю посимвольно из массива и складываю  {str1[0]+str1[3]+str1[4]}"); // число - сумма кодов символов 3263
-            // выводит число т.к. выполняется операция сложения между символами, а не строками => соответственно берутся их коды
+          Console.WriteLine($"Читаю посимвольно как из массива {str1[0]} {str1[3]} + {str1[4]}"); // "сок"
+          Console.WriteLine($"Читаю посимвольно из массива и складываю  {str1[0]+str1[3]+str1[4]}"); // число - сумма кодов символов 3263
+          // выводит число т.к. выполняется операция сложения между символами, а не строками => соответственно берутся их коды
 
-            Console.WriteLine("Читаю посимвольно по одному  {0}{1}{2}", str1[0],str1[3],str1[4]); // "сок"
+          Console.WriteLine("Читаю посимвольно по одному  {0}{1}{2}", str1[0],str1[3],str1[4]); // "сок"
 
-            Console.WriteLine($"Сравнение str1==str2 {str1==str2}");
-            Console.WriteLine($"Сравнение CompareTo {str1.CompareTo(str2)} и {str2.CompareTo(str1)} "); // стравнение выполянется попозиционно 
-            Console.WriteLine($"Сравнение Compare {String.Compare(str1,str2)} и {String.Compare(str2,str1)} ");
+          Console.WriteLine($"Сравнение str1==str2 {str1==str2}");
+          Console.WriteLine($"Сравнение CompareTo {str1.CompareTo(str2)} и {str2.CompareTo(str1)} "); // стравнение выполянется попозиционно 
+          Console.WriteLine($"Сравнение Compare {String.Compare(str1,str2)} и {String.Compare(str2,str1)} ");
 
-            string strSpl = str2.Substring(9,3); // вырезали "Стр"
-            Console.WriteLine(strSpl);
+          string strSpl = str2.Substring(9,3); // вырезали "Стр"
+          Console.WriteLine(strSpl);
 
-            Console.WriteLine(str1.GetType());
+          Console.WriteLine(str1.GetType()); //
+          */
+            #endregion
 
-                  
 
+            #region Урок 3 /логический тип/ 
+            //bool yes = true;
+            //bool no = false;
+            //Console.WriteLine($"1==2 ? { 1 == 2}");
+            //Console.WriteLine($"1>=2 ?  { 1 >= 2}");
+            //Console.WriteLine($"1<=2  ? { 1 <= 2}");
+            //Console.WriteLine($"1>2  ? { 1 > 2}");
+            //Console.WriteLine($"1<2  ? { 1 < 2}");
+            
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            if (name == "Игорь")
+            {
+                Console.WriteLine("Угадал! Я Игорь!");
+            }
+            else
+            {
+                Console.WriteLine("Ничего подобного! Я Игорь!");
+            }
+
+            switch (name)
+            {
+                case "Игорь":
+                    name = "Игореха";
+                    break;
+                case "Саша":
+                    name = "Сяня";
+                    break;
+                case "Федор":
+                    name = "Федя";
+                    break;
+                default:
+                    name = "ХЗ кто ты";
+                    break;
+            }
+
+            Console.WriteLine($"Здорово, {name}! Идем в магазин! ");
+            int totalMoney = 1000;
+            int minPrice = 20;
+            int maxPrice = 500;
+            Console.WriteLine("Введите цену товара");
+            while (totalMoney>minPrice)
+            {
+                int price = 0;
+                if (int.TryParse(Console.ReadLine(), out price))
+                {
+                    if (price>=minPrice&&price<=maxPrice&&price<totalMoney)
+                    {
+                        totalMoney-=price ;
+                        Console.WriteLine($"Покупаем, остаток {totalMoney}");
+                    }
+                    else
+                    {
+                        if (price > maxPrice | price > totalMoney)
+                        {
+                            Console.WriteLine("Дороговато! Не покупаем");
+                        }
+                        else if (price < minPrice)
+                        {
+                            Console.WriteLine("Слишком дешевый товар нам не нужен!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Закончилось бабло! Остаток {0}",totalMoney);
+                            break;
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Что-то не то ввели, надо число!");
+                }
+
+            }
+            
 
             #endregion
         }
