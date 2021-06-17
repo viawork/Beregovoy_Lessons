9231634cb580d589c029798340e2c53f2415ad63
VIA <base_admin@sheriff.md>
2021-06-17 16:17:57 +0300

    ООП за час

--- TestLesson/Program.cs
+++ TestLesson/Program.cs
@@ -6,8 +6,73 @@ namespace TestLesson
 {
     class Program
     {
+        class Car
+        {
+            public static string type = "Автомобиль"; // статическая переменная для всех экземпляров 
+            public string regNum = "<Номер>"; // видимость в проекте
+            private string model = "Mersedes";                          // видимость в классе
+            protected int maxSpeed = 100;                               // видимость в классе и наследниках
+            public Car(string regNum, string model, int maxSpeed = 200) //конструктор        
+            {
+                this.regNum = regNum;
+                this.model = model;
+                this.maxSpeed = maxSpeed;
+            }
+            public Car() { }
+
+            public override string ToString()
+            {
+                string str = $"{type} {model} {regNum} скорость:{maxSpeed}";
+                //Console.WriteLine(str);
+                return str;
+            }
+            public void Print() => Console.WriteLine(this.ToString());
+
+        }
+
+
+        class Bus : Car //задаю класс наследник 
+        {
+            private byte seatsNum = 10;
+            public Bus(string regNum2, string model2, int maxSpeed2, byte seats) : base(regNum2, model2, maxSpeed2)
+            {
+                seatsNum = seats;   // разные идентификаторы переменных поэтому можно и без this
+                maxSpeed = 150;     // protected переменная доступна и в наследнике и переопределится в наследник после инициализации Bus
+                maxSpeed2 = 250;    // уже поздно не сработает в base()
+                type = "Бусик";
+            }
+
+            public override string ToString() // добавляю seatsNum
+            {
+                string str = base.ToString() + $" мест:{seatsNum} " +;
+                //Console.WriteLine(str);
+                return str;
+            }
+        }
         static void Main(string[] args)
         {
+            //Console.WriteLine("Начинаем");
+            #region Классы
+
+            // определяю глобальные поля после инициализации
+
+            Car def = new Car();
+            def.regNum = "TT55EE";
+            def.Print();
+
+            Car.type = "Машина";
+            Car toyota = new Car(regNum: "T532NE", model: "Toyota");
+            toyota.Print();
+
+
+            Bus sprinter = new Bus("T512EE", "Mers Sprinter", seats: 20, maxSpeed2: 200);
+            sprinter.Print();
+
+            Bus ikarus = new Bus("T123", "Ikarus", seats: 50, maxSpeed2: 120);
+            Car.type = "Автобус"; // переопределяю дефолтный тип
+            ikarus.Print();
+
+            #endregion
 
             #region Урок1 /числа/
             /*
@@ -67,7 +132,6 @@ namespace TestLesson
           */
             #endregion
 
-
             #region Урок 3 /логический тип/ 
             //bool yes = true;
             //bool no = false;
@@ -76,6 +140,10 @@ namespace TestLesson
             //Console.WriteLine($"1<=2  ? { 1 <= 2}");
             //Console.WriteLine($"1>2  ? { 1 > 2}");
             //Console.WriteLine($"1<2  ? { 1 < 2}");
+
+
+            //string name = Console.ReadLine();
+
             //switch (name)
             //{
             //    case "Игорь":
@@ -93,53 +161,53 @@ namespace TestLesson
             //}
 
             //Console.WriteLine($"Здорово, {name}! Идем в магазин! ");
-            int totalMoney = 1000;
-            int minPrice = 20;
-            int maxPrice = 500;
+            //int totalMoney = 1000;
+            //int minPrice = 20;
+            //int maxPrice = 500;
 
-            List<int> log = new List<int>();// список покупок
+            //List<int> log = new List<int>();// список покупок
 
-            Console.WriteLine("Введите цену товара");
-            while (totalMoney > minPrice)
-            {
-                int price = 0;
-                if (int.TryParse(Console.ReadLine(), out price))
-                {
-                    if (price >= minPrice && price <= maxPrice && price < totalMoney)
-                    {
-                        log.Add(price);
-                        totalMoney -= price;
-                        Console.WriteLine($"Покупаем, остаток {totalMoney}");
-                    }
-                    else
-                    {
-                        if (price > maxPrice | price > totalMoney)
-                        {
-                            Console.WriteLine("Дороговато! Не покупаем");
-                        }
-                        else if (price < minPrice)
-                        {
-                            Console.WriteLine("Слишком дешевый товар нам не нужен!");
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Что-то не то ввели, надо число!");
-                }
-            }
-            for (int i = 0; i < log.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}) Куплено на сумму {log[i]}");
-            }
-            Console.WriteLine($"Всего покупок {log.Count}"); log.RemoveRange(1, 3);
-            Console.WriteLine("Закончилось бабло! Остаток {0}", totalMoney);
-            
-            Console.WriteLine("удалили 3 покупки, осталось" + log.Count);
-            foreach (int elem in log)
-            {
-                Console.WriteLine(elem);
-            }
+            //Console.WriteLine("Введите цену товара");
+            //while (totalMoney > minPrice)
+            //{
+            //    int price = 0;
+            //    if (int.TryParse(Console.ReadLine(), out price))
+            //    {
+            //        if (price >= minPrice && price <= maxPrice && price < totalMoney)
+            //        {
+            //            log.Add(price);
+            //            totalMoney -= price;
+            //            Console.WriteLine($"Покупаем, остаток {totalMoney}");
+            //        }
+            //        else
+            //        {
+            //            if (price > maxPrice | price > totalMoney)
+            //            {
+            //                Console.WriteLine("Дороговато! Не покупаем");
+            //            }
+            //            else if (price < minPrice)
+            //            {
+            //                Console.WriteLine("Слишком дешевый товар нам не нужен!");
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Что-то не то ввели, надо число!");
+            //    }
+            //}
+            //for (int i = 0; i < log.Count; i++)
+            //{
+            //    Console.WriteLine($"{i + 1}) Куплено на сумму {log[i]}");
+            //}
+            //Console.WriteLine($"Всего покупок {log.Count}"); log.RemoveRange(1, 3);
+            //Console.WriteLine("Закончилось бабло! Остаток {0}", totalMoney);
+
+            //Console.WriteLine("удалили 3 покупки, осталось" + log.Count);
+            //foreach (int elem in log)
+            //{
+            //    Console.WriteLine(elem);
+            //}
 
             #endregion
         }
